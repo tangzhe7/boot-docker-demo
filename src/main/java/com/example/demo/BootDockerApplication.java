892bc24de1ff3bc201a6b25b7dea2090fc1d9505
caicai <jiangchong@qingclass.com>
2018-01-25 19:51:30 +0800

    error-not-use

--- src/main/java/com/example/demo/BootDockerApplication.java
+++ src/main/java/com/example/demo/BootDockerApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BootDockerApplication
 {
+	
+	@Value("${app.env}")
+	private String env;
 
 	@RequestMapping("/")
 	public String home()
 	{
-		return "Hello Docker World";
+		return "Hello Docker World-this env "+env;
 	}
 
 	public static void main(String[] args)
